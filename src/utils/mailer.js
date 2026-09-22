f325c6e1bcf5df5f0e8e326506f03b22ab5a9aed
Julian Iglesias <julianiglesias2482@gmail.com>
2026-09-22 00:46:38 -0300

    Entrega 7 completa

--- src/utils/mailer.js
+++ src/utils/mailer.js
@@ -1,7 +1,9 @@
 import nodemailer from 'nodemailer'
+import 'dotenv/config'
 
 const transporter = nodemailer.createTransport({
     host:process.env.mail_host, port:Number(process.env.mail_port),secure:false,auth:{user:process.env.mail_user,pass:process.env.mail_pass}
 })
 
-export const sendTicketConfirmation=async({to,eventTitle,reservationCode,quantity})=>{await transporter.sendMail({from: process.env.mail_from,to,subject:`Confirmación de inscripción - ${eventTitle}`,text:`Tu inscripción fue confimada. Evento:${eventTitle} Cantidad: ${quantity} Codigo de reserva: ${reservation}`})}
+export const sendTicketConfirmation=async({to,eventTitle,reservationCode,quantity})=>{await transporter.sendMail({from: process.env.mail_from,to,subject:`Confirmación de inscripción - ${eventTitle}`,text:`Tu inscripción fue confimada. Evento:${eventTitle} Cantidad: ${quantity} Codigo de reserva: ${reservationCode}`})}
+
